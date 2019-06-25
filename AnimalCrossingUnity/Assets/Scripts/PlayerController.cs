0e2b3746e4a683be7225f4a59b83dadaa32b46c6
Thanut Art Parkeenvincha <art.parkeenvincha@gmail.com>
2019-06-24 23:59:12 -0700

    The beginning of something beautiful. Created Unity environment and added some skeleton code

---
+++ AnimalCrossingUnity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+   
+        gameObject.transform.position = new Vector3(transform.position.x + (h * speed), transform.position.y, transform.position.z + (v * speed));
+    }
+}
